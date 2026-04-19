80f7f5dc55a0de32de0591d287206344a2b2dc9a
Ryanmccthy19 <mccarthy.r3@northeastern.edu>
2026-04-18 20:14:02 -0400

    Updated navigation

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -62,6 +62,24 @@ def professor_assignments_nav():
 def data_analyst_home_nav():
     st.sidebar.page_link("pages/17_Data_Analyst_Home.py", label="Data Analyst Home", icon="🖥️")
 
+def data_analyst_correlations_nav():
+    st.sidebar.page_link("pages/31_Analyst_Correlations.py", label = "Correlations")
+
+def data_analyst_trends_nav():
+    st.sidebar.page_link("pages/32_Analyst_Trend_Flags.py", label = "Flag Trends")
+
+def data_analyst_filtering_nav():
+    st.sidebar.page_link("pages/33_Analyst_Filtering.py", label = "Filter Student Data")
+
+def data_analyst_breakdown_nav():
+    st.sidebar.page_link("pages/34_Analyst_Category_Breakdown.py", label = "Category Breakdown")
+
+def data_analyst_sharing_nav():
+    st.sidebar.page_link("pages/35_Analyst_Institution_Sharing.py", label = "Institution Sharing")
+
+def data_analyst_export_nav():
+    st.sidebar.page_link("pages/36_Analyst_Export.py", label = "Export Data")
+
 
 # ---- Role: System Administrator ---------------------------------------------
 
@@ -69,10 +87,20 @@ def admin_home_nav():
     st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
 
 
-def ml_model_mgmt_nav():
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
+def admin_dupes_nav():
+    st.sidebar.page_link("pages/22_Admin_Duplicates.py", label = "Duplicates")
+
+def admin_records_nav():
+    st.sidebar.page_link("pages/23_Admin_Records.py", label = "Incorrect Records")
+
+def admin_usage_nav():
+    st.sidebar.page_link("pages/26_Admin_Usage.py", label="System Usage")
+
+def admin_categories_nav():
+    st.sidebar.page_link("pages/24_Admin_Categories.py", label="Update Categories")
+
+def admin_archive_nav():
+    st.sidebar.page_link("pages/27_Admin_Archive.py", label="Archive")
 
 # ---- Sidebar assembly -------------------------------------------------------
 
@@ -109,11 +137,21 @@ def SideBarLinks(show_home=False):
 
         if st.session_state["role"] == "data analyst":
             data_analyst_home_nav()
-            ml_model_mgmt_nav()
+            data_analyst_correlations_nav()
+            data_analyst_trends_nav()
+            data_analyst_filtering_nav()
+            data_analyst_breakdown_nav()
+            data_analyst_sharing_nav()
+            data_analyst_export_nav()
+
 
         if st.session_state["role"] == "system admin":
             admin_home_nav()
-            ml_model_mgmt_nav()
+            admin_dupes_nav()
+            admin_records_nav()
+            admin_usage_nav()
+            admin_categories_nav()
+            admin_archive_nav()
 
     # About link appears at the bottom for all roles
     about_page_nav()
